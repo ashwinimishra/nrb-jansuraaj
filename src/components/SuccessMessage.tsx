d877f8b00a270721ada979ecfaa3947865c30385
ashwinimishra <31121327+ashwinimishra@users.noreply.github.com>
2025-03-13 07:55:01 +0530

    Prashant Image Change

--- src/components/SuccessMessage.tsx
+++ src/components/SuccessMessage.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { User } from '../types';
 import { CheckCircle, Copy, Share2, User as UserIcon, Facebook } from 'lucide-react';
 import { toast } from 'react-toastify';
-import prashantImage from '../assets/Prashant.jpg';
+import prashantImage from '../assets/Prashant.jpeg';
 import ShareFBPost from './ShareFBPost';
 
 interface SuccessMessageProps {
